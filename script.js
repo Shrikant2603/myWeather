3285e2219e13e911a2423ce2c71ebbeb604457ab
Shrikant2603 <shrikantparinge@gmail.com>
2022-12-11 11:47:22 +0530

    Second commit

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function searchByCity(){
 
 function weatherReport(data){
 
-    var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
+    var urlcast= `https://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
 
     fetch(urlcast).then((res)=>{
         return res.json();
@@ -57,7 +57,7 @@ function weatherReport(data){
         console.log(data.weather[0].description)
         
         let icon1= data.weather[0].icon;
-        let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+        let iconurl= "https://api.openweathermap.org/img/w/"+ icon1 +".png";
         document.getElementById('img').src=iconurl
     })
 
